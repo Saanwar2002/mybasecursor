8862cd5deba016c2625b1771b261e9ef609bbbb0
Shabz <shabz3038@gmail.com>
2025-06-07 15:50:01 +0000

    Still this

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
 
-export default function MinimalTestPage() {
-  return (
-    <div>
-      <h1>Minimal Test Page</h1>
-      <p>If you see this, the basic page component is working.</p>
-    </div>
-  );
-}
+import LandingPage from './(marketing)/page';
+
+export default LandingPage;
