0793c22a779276fd280f799ec30d36c2fbe00001
Shabz <shabz3038@gmail.com>
2025-06-07 00:24:53 +0000

    this

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  /*
   experimental: {
+    // This 'allowedDevOrigins' key is unrecognized in Next.js 14.2.x
+    // and causes a configuration error. Removing it.
     allowedDevOrigins: [
         "https://9004-firebase-studio-1748687301287.cluster-ombtxv25tbd6yrjpp3lukp6zhc.cloudworkstations.dev"
     ],
   },
+  */
   images: {
     remotePatterns: [
       {
