3d94eb444b44bc5af19d140b65733880a150f8ce
Shabz <shabz3038@gmail.com>
2025-06-09 08:59:20 +0000

    little better, can you reduce more by same ratio as 1st time

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -7,10 +7,11 @@ export default function MarketingLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <PublicHeader />
-      <main className="flex-grow container mx-auto px-4 pt-4 pb-8 flex justify-center items-center"> {/* Changed py-8 to pt-4 pb-8 */}
+      <main className="flex-grow container mx-auto px-4 pt-2 pb-8 flex justify-center items-center"> {/* Changed pt-4 to pt-2 */}
         {children}
       </main>
       <PublicFooter />
     </div>
   );
 }
+
