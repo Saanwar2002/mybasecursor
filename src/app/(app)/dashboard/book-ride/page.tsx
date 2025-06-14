a2002af0fd4decc7b10f36e86d9c2cf181485751
Shabz <shabz3038@gmail.com>
2025-06-14 12:00:41 +0000

    At this stage I think we need to zoom in from 16 to 18 level so user can

--- src/app/(app)/dashboard/book-ride/page.tsx
+++ src/app/(app)/dashboard/book-ride/page.tsx
@@ -1626,7 +1626,7 @@ export default function BookRidePage() {
 
   const { mapCenterForDisplay, mapZoomForDisplay } = useMemo(() => {
     if (showGpsSuggestionAlert && suggestedGpsPickup?.coords) {
-        return { mapCenterForDisplay: suggestedGpsPickup.coords, mapZoomForDisplay: 14 };
+        return { mapCenterForDisplay: suggestedGpsPickup.coords, mapZoomForDisplay: 18 };
     }
     if (pickupCoords) {
         return { mapCenterForDisplay: pickupCoords, mapZoomForDisplay: 14 };
@@ -2690,6 +2690,7 @@ const handleProceedToConfirmation = async () => {
     
 
     
+
 
 
 
